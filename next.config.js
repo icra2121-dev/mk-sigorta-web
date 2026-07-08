2bac431f5d4527e284e7d56b9be33d002cb19df4
ismail zengin <icra2121@gmail.com>
2026-07-08 08:56:24 +0300

    Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  i18n: {
+    locales: ['tr', 'en'],
+    defaultLocale: 'tr',
+  },
+}
+
+module.exports = nextConfig
